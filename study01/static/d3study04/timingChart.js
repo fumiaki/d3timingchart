cfae4fd8a0229feaf490abf7f8c49e3dcde469b4
fumiaki <fumiaki.oshima@gmail.com>
2017-10-18 01:30:22 +0900

    chartのY軸offsetのロジック作成中

--- study01/static/d3study04/timingChart.js
+++ study01/static/d3study04/timingChart.js
@@ -7,7 +7,7 @@ var TimingChart = (function(d3) {
   var _xScale = d3.scaleLinear();
   var _yScale = d3.scaleLinear();
 
-  var _data = {};
+  var _data = undefined;
 
   function _createConfigObject(configParam) {
 
@@ -88,10 +88,10 @@ var TimingChart = (function(d3) {
 
     //###### TEST ########
     _xAxis.selectAll(".tick")
-      .data([20, 50, 70]);
+      .data([1, 5, 10]);
 
     _yAxis.selectAll(".tick")
-      .data([0.1, 0.5, 0.8]);
+      .data([1, 2, 3]);
 
     //####################
 
@@ -100,12 +100,46 @@ var TimingChart = (function(d3) {
 
   function _setData(data) {
     _data = data;
+    _data.forEach(function(v, i, a){
+      _padData(v.data);
+      _offsetData(v.data, v.yOffset)
+    });
+
     _plotPane.selectAll("path")
       .data(_data)
-      .enter().append("path")
-      .classed("dragged", false);
+      .enter().append("path");
+
+      if(!_data) {
+        return;
+      }
+
+      var maxArray = _data.map(function(elem) {
+        return d3.max(elem.data, function(d){return d.v});
+        //return Math.max.apply(null, elem.data.map(function(elm2) {
+        //  return elm2.v;
+        //}));
+      });
+      console.log(maxArray);
+
+
+    _xScale
+      .domain([0, 13]);
+
+    _yScale
+      .domain([0, 10]);
 
     _updateDisplay();
+  }
+
+  // チャートの前後に予備データ追加（塗りつぶしの始点をY=0に合わせるため
+  function _padData(data) {
+    data.unshift({t:d3.min(data, function(d){return d.t}), v:0});
+    data.push({t:d3.max(data, function(d){return d.t}), v:0});
+  }
+
+  // 複数のチャートを縦に並べるため、チャート毎にY軸の値をOffsetする
+  function _offsetData(data, offset) {
+    data.forEach(function(v, i, a){v.v += offset});
   }
 
   function _updateDisplay() {
@@ -113,12 +147,12 @@ var TimingChart = (function(d3) {
 
     //#### TEST ######
     _plotPane.select("#test")
-      .attr("cx", _xScale(50))
-      .attr("cy", _yScale(0.5))
+      .attr("cx", _xScale(10))
+      .attr("cy", _yScale(3))
       .attr("r", _config.zoom.x * 40);
     _plotPane.select("#test2")
-      .attr("x", _xScale(60))
-      .attr("y", _yScale(0.4));
+      .attr("x", _xScale(12))
+      .attr("y", _yScale(5));
 
     _xAxis.selectAll("g")
       .attr("transform", function(d) {return "translate(" + _xScale(d) + ", 0)"});
@@ -133,17 +167,15 @@ var TimingChart = (function(d3) {
 
   function _setScale() {
     _xScale
-      .domain([0, 100])
       .range([
         _config.offset.x * _config.zoom.x,
         (_config.offset.x + _config.plotPane.width) * _config.zoom.x
       ]);
 
     _yScale
-      .domain([0, 1])
       .range([
-        _config.offset.y * _config.zoom.y,
-        (_config.offset.y + _config.plotPane.height) * _config.zoom.y
+        (_config.offset.y + _config.plotPane.height) * _config.zoom.y,
+        _config.offset.y * _config.zoom.y
       ]);
   }
 
@@ -152,10 +184,10 @@ var TimingChart = (function(d3) {
       .x(function(d) {return _xScale(d.t)})
       .y(function(d) {return _yScale(d.v)});
 
-    _plotPane.select("path")
+    _plotPane.selectAll("path")
       //.transition()
       .attr("d", function(d){return pathString(d.data)})
-      .style("fill", function(d){d.color})
+      .style("fill", function(d){return d.color})
       //.style("marker-mid", "url('#chartMarker')")
       ;
   }
